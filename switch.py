344e24b6ea89bf0c620c48060d9a129840802f60
BoppreH <boppreh@gmail.com>
2013-10-22 00:59:33 -0200

    Add webservice

--- switch.py
+++ switch.py
@@ -1,6 +1,7 @@
 from subprocess import Popen as start
 from os import path
 from requests import get, exceptions
+from git import Repo
 
 class Application(object):
     def __init__(self, project, executable, port):
@@ -8,6 +9,7 @@ class Application(object):
         self.project_dir = path.join('..', self.project)
         self.executable = path.join(self.project_dir, executable)
         self.port = port
+        self.process = None
 
     def is_running(self):
         try:
@@ -16,8 +18,33 @@ class Application(object):
         except (exceptions.ConnectionError, exceptions.Timeout):
             return False
 
-    def exists(self):
+    def is_started(self):
+        return self.process is not None
+
+    def start(self):
+        self.process = start(['pythonw', self.executable], cwd=self.project_dir)
+
+    def kill(self):
+        self.process.kill()
+        self.process = None
+
+    def is_installed(self):
         return path.isdir(self.project_dir) and path.isfile(self.executable)
+
+    def install(self):
+        project_url = project_url_template.format(application.project)
+        Repo.clone_from(project_url, application.project_dir)
+
+    def status(self):
+        if self.is_started():
+            return 'Started'
+        elif self.is_running():
+            return 'Already running'
+        elif self.is_installed():
+            return 'Stopped'
+        else:
+            return 'Not installed'
+
 
 applications = [Application('scheduler', 'schedule_notifier.pyw', 2340),
                 Application('typist', 'typist.pyw', 2341),
@@ -31,10 +58,29 @@ applications = [Application('scheduler', 'schedule_notifier.pyw', 2340),
 
 project_url_template = 'https://github.com/boppreh/{}.git'
 
-if __name__ == '__main__':
-    from git import Repo
+def install_all():
     for application in applications:
-        if not application.exists():
-            print application.project
-            project_url = project_url_template.format(application.project)
-            Repo.clone_from(project_url, application.project_dir)
+        if not application.is_installed():
+            application.install()
+
+def start_all():
+    install_all()
+
+    for application in applications:
+        if not application.is_running():
+            print 'Starting', application.project
+            application.start()
+
+
+if __name__ == '__main__':
+    from simpleserver import serve
+
+    get_services = {}
+    post_services = {'start_all': start_all, 'install_all': install_all}
+    for application in applications:
+        sub_service = {'start': application.start,
+                       'stop': application.kill}
+        post_services[application.project] = sub_service
+        get_services[application.project] = application.status
+
+    serve(get_services, post_services, port=2348)
