5b682ef44af9d95d9f87fe0f59c02f71563fd658
BoppreH <boppreh@gmail.com>
2013-10-22 00:28:16 -0200

    Add `is_running` method

--- switch.py
+++ switch.py
@@ -1,11 +1,19 @@
 from subprocess import Popen as start
 from os import path
+from requests import get, exceptions
 
 class Application(object):
     def __init__(self, project, executable, port):
         self.project = project
         self.executable = executable
         self.port = port
+
+    def is_running(self):
+        try:
+            url = 'http://localhost:' + str(self.port)
+            return 200 <= get(url, timeout=0.001).status_code < 300
+        except (exceptions.ConnectionError, exceptions.Timeout):
+            return False
 
     def exists(self):
         project_dir = path.join('..', self.project)
@@ -24,5 +32,4 @@ applications = [Application('scheduler', 'schedule_notifier.pyw', 2340),
 
 if __name__ == '__main__':
     for application in applications:
-        if not application.exists():
-            print application.project
+        print application.project, application.is_running()
